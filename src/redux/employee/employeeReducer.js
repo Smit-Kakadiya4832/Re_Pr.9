11d4130e7d81dcf09c15c80fabf771b73e1facc4
SMIT\smit <smitkakadiya2003@gmail.com>
2023-09-20 15:40:32 +0530

    first commit

--- src/redux/employee/employeeReducer.js
+++ src/redux/employee/employeeReducer.js
@@ -1,34 +1,48 @@
 import { ADD_EMPLOYEE, UPDATE_EMPLOYEE, DELETE_EMPLOYEE } from './employeeTypes';
-import { employeesData } from '../../data'
+import { employeesData } from '../../data';
+
+// Load employees from localStorage or use initial data
+const savedEmployees = JSON.parse(localStorage.getItem('crud')) || employeesData;
 
 const initialState = {
-    employees: employeesData,
-    error: null
-}
+  employees: savedEmployees,
+  error: null,
+};
 
 export const employeeReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_EMPLOYEE:
-            return {
-                ...state,
-                employees: [...state.employees, action.employee]
-            };
-        case UPDATE_EMPLOYEE:
-            return {
-                ...state,
-                employees: state.employees.map(employee => {
-                    if (employee.id === action.id) {
-                        return { ...employee, ...action.employee }
-                    }
-                    return employee;
-                })
-            };
-        case DELETE_EMPLOYEE:
-            return {
-                ...state,
-                employees: state.employees.filter(employee => employee.id !== action.id)
-            };
-        default:
-            return state;
-    }
-}
+  let updatedEmployees;
+
+  switch (action.type) {
+    case ADD_EMPLOYEE:
+      updatedEmployees = [...state.employees, action.employee || {}];
+      localStorage.setItem('crud', JSON.stringify(updatedEmployees));
+      return {
+        ...state,
+        employees: updatedEmployees,
+      };
+
+    case UPDATE_EMPLOYEE:
+      updatedEmployees = state.employees.map((employee) => {
+        if (employee.id === action.id) {
+          return { ...employee, ...(action.employee || {}) };
+        }
+        return employee;
+      });
+      localStorage.setItem('crud', JSON.stringify(updatedEmployees));
+      return {
+        ...state,
+        employees: updatedEmployees,
+      };
+
+    case DELETE_EMPLOYEE:
+      updatedEmployees = state.employees.filter((employee) => employee.id !== action.id);
+      localStorage.setItem('crud', JSON.stringify(updatedEmployees));
+      return {
+        ...state,
+        employees: updatedEmployees,
+      };
+
+    default:
+      return state;
+  }
+};
